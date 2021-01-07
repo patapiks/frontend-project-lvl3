791ec038aa845592c8a77bf13a1873f830446894
Mona Lisa <patapiks@mail.ru>
2021-01-07 22:56:42 +0300

    fail

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
+import 'bootstrap/dist/css/bootstrap-grid.css';
+import 'bootstrap/dist/css/bootstrap-utilities.css';
+import 'bootstrap/js/dist/alert';
+import 'jquery';
+
 const ex = () => {
   const point = document.querySelector('#point');
   point.innerHTML = '<h1>Hello!</h1>';
+  const newAlert = document.createElement('div');
+  newAlert.classList.add('alert', 'alert-primary');
+  newAlert.setAttribute('role', 'alert');
+  newAlert.innerHTML = 'example';
+  document.body.append(newAlert);
 };
 
 ex();
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,10 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
     ],
   },
   plugins: [
