aae3176fa9e21d1de7faa6b33d492b12d512d78c
Mona Lisa <patapiks@mail.ru>
2021-01-08 13:35:23 +0300

    First step

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-import 'bootstrap/dist/css/bootstrap-grid.css';
-import 'bootstrap/dist/css/bootstrap-utilities.css';
-import 'bootstrap/js/dist/alert';
-import 'jquery';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
 const ex = () => {
-  const point = document.querySelector('#point');
-  point.innerHTML = '<h1>Hello!</h1>';
+  // const point = document.querySelector('#point');
+  // point.innerHTML = '<h1>Hello!</h1>';
   const newAlert = document.createElement('div');
   newAlert.classList.add('alert', 'alert-primary');
   newAlert.setAttribute('role', 'alert');
   newAlert.innerHTML = 'example';
-  document.body.append(newAlert);
+  // document.body.append(newAlert);
 };
 
 ex();
