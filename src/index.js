8df37fa7607633f72b833f41fc1cf0bfd06b3af5
Mona Lisa <patapiks@mail.ru>
2021-01-08 13:36:56 +0300

    First step

--- src/index.js
+++ src/index.js
@@ -1,14 +1,2 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-
-const ex = () => {
-  // const point = document.querySelector('#point');
-  // point.innerHTML = '<h1>Hello!</h1>';
-  const newAlert = document.createElement('div');
-  newAlert.classList.add('alert', 'alert-primary');
-  newAlert.setAttribute('role', 'alert');
-  newAlert.innerHTML = 'example';
-  // document.body.append(newAlert);
-};
-
-ex();
