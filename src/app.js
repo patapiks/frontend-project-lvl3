d00bb7b2c655e6eca6f91c9e6c6a6c012d63bea2
Mona Lisa <patapiks@mail.ru>
2021-01-22 14:09:18 +0300

    fix for test

--- src/app.js
+++ src/app.js
@@ -62,7 +62,7 @@ export default () => {
           input.focus();
         })
         .catch((error) => {
-          state.errors = error.status;
+          state.errors = error;
           watchedState.state = 'failed';
           /*
           if (error.message === 'Network Error' && !error.response) {
