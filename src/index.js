87e00e716b84fcbaa7444589585edc68a2f97bd9
Mona Lisa <patapiks@mail.ru>
2021-01-27 15:32:33 +0300

    delete comments

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
-// import i18next from 'i18next';
 import runApp from './app';
-// import resources from './locales/index';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/js/dist/modal';
 
-/*
-const { en, ru } = resources;
-i18next
-  .init({
-    lng: 'en',
-    debug: true,
-    resources: {
-      en,
-      ru,
-    },
-  })
-  .then(() => runApp());
-  */
 runApp();
